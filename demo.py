48eb613024cbc52228361bbd57d89caf031ac4e8
Aayush Gavasne <aayushgavasne290@gmail.com>
2026-09-12 16:29:33 +0500

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,7 @@
+
+# write a program and determine whether the given  age is  adult
+age = int(input("enter the age "))
+if age > 18:
+    print("You are an adult")
+else:
+    print("You are not an adult")
